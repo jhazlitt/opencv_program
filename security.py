4350196ec129513de242e55008fd3d98ab733c54
jhazlitt <bagelday@gmail.com>
2015-10-12 21:51:04 -0600

    Merged with test_branch

--- security.py
+++ security.py
@@ -1,150 +1,4 @@
-import numpy as np
-import cv2
-import sqlite3
-import urllib
-import time
-import os
-
-# Get password for database
-conn = sqlite3.connect('/home/john/opencv_database.db')
-c = conn.cursor()
-passCode = c.execute('select password from passwords limit 1;')
-
-# Strip extra characters from the query result
-for passWd in passCode:
-	passWd = str(passWd)
-	passWd = passWd[3:len(passWd)-3]
-
-# Specify the video to be captured
-cap = cv2.VideoCapture("http://10.0.0.6:8090/videostream.asf?user=admin&pwd=" + passWd + "&resolution=32&rate=0&.mpg")
-
-# Get the starting time and starting video number
-startTime = time.time()
-videoNumber = 1
-
-# Codec and VideoWriter object for saving the video
-fourcc = cv2.cv.CV_FOURCC(*'XVID')
-out = cv2.VideoWriter(str(videoNumber) + '.avi',fourcc, 15, (640,480))
-
-fgbg = cv2.BackgroundSubtractorMOG()
-
-motionDetectedFrameCount = 0
-motionDetected = False
-mute = False
-# While the camera is recording
-while(1):
-	# Check for any keys that were pressed
-	k = cv2.waitKey(30) & 0xff
-	if k == ord('q') or k == 27:
-		break
-	elif k == ord('k'):
-		# Generate a new background
-		fgbg = cv2.BackgroundSubtractorMOG()
-	elif k == ord('m'):
-		mute = not mute 
-	elif k == ord('w'):
-		# Move camera up
-		urllib.urlopen("http://admin:" + passWd + "@10.0.0.6:8090/decoder_control.cgi?command=0")
-		urllib.urlopen("http://admin:" + passWd + "@10.0.0.6:8090/decoder_control.cgi?command=1")
-	elif k == ord('a'):
-		# Move camera left
-		urllib.urlopen("http://admin:" + passWd + "@10.0.0.6:8090/decoder_control.cgi?command=4")
-		urllib.urlopen("http://admin:" + passWd + "@10.0.0.6:8090/decoder_control.cgi?command=1")
-	elif k == ord('s'):
-		# Move camera down
-		urllib.urlopen("http://admin:" + passWd + "@10.0.0.6:8090/decoder_control.cgi?command=2")
-		urllib.urlopen("http://admin:" + passWd + "@10.0.0.6:8090/decoder_control.cgi?command=1")
-	elif k == ord('d'):
-		# Move camera right	
-		urllib.urlopen("http://admin:" + passWd + "@10.0.0.6:8090/decoder_control.cgi?command=6")
-		urllib.urlopen("http://admin:" + passWd + "@10.0.0.6:8090/decoder_control.cgi?command=1")
-
-	# Read the current frame from the camera
-	ret, frame = cap.read()
-
-	# If there has been motion detected for more than a specified number of frames, generate a new mask
-	if motionDetectedFrameCount > 1:
-		fgbg = cv2.BackgroundSubtractorMOG()
-		motionDetectedFrameCount = 0
-
-	# Apply the mask to the frame
-	fgmask = fgbg.apply(frame)
-	kernel = np.ones((5,5),np.uint8)	
-	fgmask = cv2.erode(fgmask,kernel,iterations = 1)
-	fgmask = cv2.dilate(fgmask,kernel,iterations = 2)
-	
-	# Find differences between the mask and frame, if any.  These are called contours
-	contours, hierarchy = cv2.findContours(fgmask,cv2.RETR_LIST,cv2.CHAIN_APPROX_NONE)
-
-	# If there are no contours an error will be thrown.  If there are contours:
-	if len(contours) != 0:
-		cnt = contours[0]
-		x,y,w,h = cv2.boundingRect(cnt)
-		minX = x
-		minY = y
-		maxX = x + w
-		maxY = y + h
-		for contour in contours:
-			x,y,w,h = cv2.boundingRect(contour)
-			if x < minX:
-				minX = x
-			elif y < minY:
-				minY = y
-			elif (x + w) > maxX:
-				maxX = (x + w)
-			elif (y + h) > maxY:
-				maxY = (y + h)
-		
-		centerX = (minX + maxX) / 2
-		centerY = (minY + maxY) / 2
-		cv2.rectangle(frame,(centerX,centerY),(centerX,centerY),(255,000,255),2)
-		cv2.rectangle(frame,(minX,minY),(maxX,maxY),(255,000,255),2)
-		motionDetectedFrameCount += 1
-		motionDetected = True
-		if mute == False:
-			os.system("aplay beep.wav")
-
-		# Record movement time of occurrence in log
-		f = open('log.txt','a')
-		f.write('Movement detected ' + time.asctime(time.localtime()) + '\n')
-		f.close()
-
-	endTime = time.time() 
-	print "endTime: " + str(endTime)
-	elapsedTime = endTime - startTime
-	print "elapsedTime: " + str(elapsedTime)
-
-	# Put a timestamp on the video frame
-	font = cv2.FONT_HERSHEY_SIMPLEX
-	cv2.putText(frame,str(time.asctime(time.localtime())),(0,25), font, 1, (0,0,0), 7)
-	cv2.putText(frame,str(time.asctime(time.localtime())),(0,25), font, 1, (255,255,255), 2)
-
-	cv2.imshow('Video',frame)
-	out.write(frame)
-
-	# Save the video after a specified number of seconds
-	if elapsedTime >= 60:
-		out.release()
-		
-		# If there was motion detected during the recording, move on to the next video number.  Otherwise write over this video
-
-		# If there are more than a specified number of videos, the count is set back to 1 so they can all be written over
-		if (videoNumber == 150) and (motionDetected == True):
-			motionDetected = False
-			videoNumber = 1
-		elif motionDetected == True:
-			motionDetected = False
-			videoNumber += 1
-
-		out = cv2.VideoWriter(str(videoNumber) + '.avi',fourcc, 12, (640,480))
-		startTime = time.time()
-cap.release()
-out.release()
-cv2.destroyAllWindows()
-
-#import urllib
-#import sqlite3
-#
+# Commands to move the camera
 #PTZ_STOP=1
 #TILT_UP=0
 #TILT_UP_STOP=1
@@ -163,15 +17,309 @@ cv2.destroyAllWindows()
 #PTZ_VPATROL_STOP=27
 #PTZ_HPATROL=28
 #PTZ_HPATROL_STOP=29
-#
-##Get password for database
-#conn = sqlite3.connect('/home/john/opencv_database.db')
-#c = conn.cursor()
-#passCode = c.execute('select password from passwords limit 1;')
-#for passWd in passCode:
-#	passWd = str(passWd)
-#	passWd = passWd[3:len(passWd)-3]
-#
-#urllib.urlopen("http://admin:" + passWd + "@10.0.0.6:8090/decoder_control.cgi?command=4")
-#
-#urllib.urlopen("http://admin:" + passWd + "@10.0.0.6:8090/decoder_control.cgi?command=1")
+
+import numpy as np
+import cv2
+import sqlite3
+import urllib
+import time
+import os
+from Tkinter import *
+
+def runCamera(cameraName):
+	movementDetectedTime = ""
+
+	# Connect to database
+	conn = sqlite3.connect('/home/john/opencv_database.db')
+	c = conn.cursor()
+
+	# Get camera values from database
+	ip = retrieveFromDatabase("ip", cameraName)
+	port = retrieveFromDatabase("port", cameraName)
+	password = retrieveFromDatabase("password", cameraName)
+
+	# Create camera url
+	mpegURL = "http://" + ip + ":" + port + "/videostream.asf?user=admin&pwd=" + password + "&resolution=32&rate=0&.mpg"
+
+	# Specify the video to be captured
+	cap = cv2.VideoCapture(mpegURL)
+
+	# Get the starting time and starting video number
+	startTime = time.time()
+	videoNumber = 1
+
+	# Codec and VideoWriter object for saving the video
+	fileSaveDirectory = retrieveDirectoryFromDB()
+
+	fourcc = cv2.cv.CV_FOURCC(*'XVID')
+	out = cv2.VideoWriter(str(fileSaveDirectory) + str(videoNumber) + '.avi',fourcc, 15, (640,480))
+
+	fgbg = cv2.BackgroundSubtractorMOG()
+
+	motionDetectedFrameCount = 0
+	motionDetected = False
+	mute = False
+	# While the camera is recording
+	while(1):
+		# Check for any keys that were pressed
+		k = cv2.waitKey(30) & 0xff
+		if k == ord('q') or k == 27:
+			break
+		elif k == ord('k'):
+			# Generate a new background
+			fgbg = cv2.BackgroundSubtractorMOG()
+		elif k == ord('m'):
+			mute = not mute 
+		elif k == ord('w'):
+			# Move camera up
+			moveCamera(password, ip, port, 0)
+		elif k == ord('a'):
+			# Move camera left
+			moveCamera(password, ip, port, 4)
+		elif k == ord('s'):
+			# Move camera down
+			moveCamera(password, ip, port, 2)
+		elif k == ord('d'):
+			# Move camera right	
+			moveCamera(password, ip, port, 6)
+		# Stop any camera movement
+		moveCamera(password, ip, port, 1)
+
+		# Read the current frame from the camera
+		ret, frame = cap.read()
+
+		# If there has been motion detected for more than a specified number of frames, generate a new mask
+		if motionDetectedFrameCount > 1:
+			fgbg = cv2.BackgroundSubtractorMOG()
+			motionDetectedFrameCount = 0
+
+		# Apply the mask to the frame
+		fgmask = fgbg.apply(frame)
+		kernel = np.ones((5,5),np.uint8)	
+		fgmask = cv2.erode(fgmask,kernel,iterations = 1)
+		fgmask = cv2.dilate(fgmask,kernel,iterations = 2)
+		
+		# Find differences between the mask and frame, if any.  These are called contours
+		contours, hierarchy = cv2.findContours(fgmask,cv2.RETR_LIST,cv2.CHAIN_APPROX_NONE)
+
+		# If there are no contours an error will be thrown.  If there are contours:
+		if len(contours) != 0:
+			cnt = contours[0]
+			x,y,w,h = cv2.boundingRect(cnt)
+			minX = x
+			minY = y
+			maxX = x + w
+			maxY = y + h
+			for contour in contours:
+				x,y,w,h = cv2.boundingRect(contour)
+				if x < minX:
+					minX = x
+				elif y < minY:
+					minY = y
+				elif (x + w) > maxX:
+					maxX = (x + w)
+				elif (y + h) > maxY:
+					maxY = (y + h)
+			
+			centerX = (minX + maxX) / 2
+			centerY = (minY + maxY) / 2
+			cv2.rectangle(frame,(centerX,centerY),(centerX,centerY),(255,000,255),2)
+			cv2.rectangle(frame,(minX,minY),(maxX,maxY),(255,000,255),2)
+			motionDetectedFrameCount += 1
+			motionDetected = True
+			if not mute:
+				os.system("aplay beep.wav")
+
+			# Record movement time of occurrence in log
+			if (movementDetectedTime != time.asctime(time.localtime())):
+				movementDetectedTime = time.asctime(time.localtime())
+				c.execute('INSERT INTO log (timestamp) VALUES ("' + time.asctime(time.localtime()) + '");')
+				conn.commit()
+				
+		endTime = time.time() 
+		print "endTime: " + str(endTime)
+		elapsedTime = endTime - startTime
+		print "elapsedTime: " + str(elapsedTime)
+
+		# Put a timestamp on the video frame
+		font = cv2.FONT_HERSHEY_SIMPLEX
+		cv2.putText(frame,str(time.asctime(time.localtime())),(0,25), font, 1, (0,0,0), 7)
+		cv2.putText(frame,str(time.asctime(time.localtime())),(0,25), font, 1, (255,255,255), 2)
+		
+		# Add MUTE text if the program is muted
+		if mute:
+			cv2.putText(frame,"MUTE",(555,475), font, 1, (0,0,255), 4)
+
+		cv2.imshow('Video',frame)
+		out.write(frame)
+
+		# Save the video after a specified number of seconds
+		if elapsedTime >= 60:
+			out.release()
+			
+			# If there was motion detected during the recording, move on to the next video number.  Otherwise write over this video
+			# If there are more than a specified number of videos, the count is set back to 1 so they can all be written over
+			if (videoNumber == 150) and (motionDetected == True):
+				motionDetected = False
+				videoNumber = 1
+			elif motionDetected == True:
+				motionDetected = False
+				videoNumber += 1
+
+			out = cv2.VideoWriter(str(fileSaveDirectory) + str(videoNumber) + '.avi',fourcc, 12, (640,480))
+			startTime = time.time()
+	cap.release()
+	out.release()
+	cv2.destroyAllWindows()
+
+def retrieveFromDatabase(value, camera):
+	# Get value from database
+	result = c.execute('SELECT ' + value + ' FROM cameras WHERE name = "' + camera + '";')
+	# Strip extra characters from the query result
+	for value in result:
+		value = str(value)
+		value = value[3:len(value)-3]
+	return value
+
+def retrieveDirectoryFromDB():
+	# Get value from database
+	result = c.execute('SELECT directory FROM save_directory;')
+	# Strip extra characters from the query result
+	for value in result:
+		value = str(value)
+		value = value[3:len(value)-3]
+	return value
+	
+def moveCamera(password, ip, port, direction):
+	direction = str(direction)
+	moveURL = "http://admin:" + password + "@" + ip + ":" + port + "/decoder_control.cgi?command=" + direction + ""
+	urllib.urlopen(moveURL)
+
+class MyApp:
+	def __init__(self, parent):
+		self.frame = Frame(parent)
+		self.frame.pack()
+		self.home()
+
+	def home(self):
+		# Remove all widgets from the frame
+		self.clearFrame()
+		rowCount = 0
+	
+		# Get any existing cameras from the database
+		cameras = c.execute('SELECT name FROM cameras;')
+		for camera in cameras:
+			camera = str(camera)
+			camera = camera[3:len(camera)-3]
+			# Add widgets for the camera
+			newCameraButton = Button(self.frame, text="" + camera + "", command = lambda name=camera:self.startCameraFeed("" + name  + ""))
+			newRemoveButton = Button(self.frame, text="Remove", command = lambda name=camera:self.removeCamera("" + name + ""))
+			# Position the widgets
+			newCameraButton.grid(row=rowCount, sticky=E)
+			newRemoveButton.grid(row=rowCount, column=1)
+			rowCount += 1
+			
+		# Add default widgets to the frame
+		self.addCameraButton = Button(self.frame, text="Add Camera", command=self.addCamera)
+		self.settingsButton = Button(self.frame, text="Settings", command=self.settings)
+		self.quitButton = Button(self.frame, text="Quit", command=self.closeWindow)
+
+		# Position the default widgets
+		self.addCameraButton.grid(row=rowCount, columnspan=2)
+		self.settingsButton.grid(row=rowCount + 1, columnspan=2)	
+		self.quitButton.grid(row=rowCount + 2, columnspan=2)
+
+	def addCamera(self):
+		# Remove all widgets from the frame
+		self.clearFrame()
+
+		# Add new widgets to the frame
+		self.nameLabel = Label(self.frame, text="Camera name:") 
+		self.ipLabel = Label(self.frame, text="IP address:")
+		self.portLabel = Label(self.frame, text="Port:")
+		self.passwordLabel = Label(self.frame, text="Password:")
+		self.nameEntry = Entry(self.frame)
+		self.ipEntry = Entry(self.frame)
+		self.portEntry = Entry(self.frame)
+		self.passwordEntry = Entry(self.frame)
+		self.addCameraButton = Button(self.frame, text="Add", command=self.writeCameraToDatabase)
+		self.backButton = Button(self.frame, text="Back", command=self.home)
+		
+		# Position the widgets
+		self.nameLabel.grid(row=0, sticky=E) 
+		self.ipLabel.grid(row=1, sticky=E)
+		self.portLabel.grid(row=2, sticky=E)
+		self.passwordLabel.grid(row=3, sticky=E)
+		self.nameEntry.grid(row=0, column=1)
+		self.ipEntry.grid(row=1, column=1)
+		self.portEntry.grid(row=2, column=1)
+		self.passwordEntry.grid(row=3, column=1)
+		self.addCameraButton.grid(row=4, columnspan=2)
+		self.backButton.grid(row=5, columnspan=2)
+
+	def settings(self):
+		# Remove all widgets from the frame
+		self.clearFrame()
+
+		result = c.execute('SELECT directory FROM save_directory;')
+		for directory in result:
+			directory = str(directory)
+			directory = directory[3:len(directory)-3]
+			 
+		# Add new widgets to the frame
+		self.saveDirectoryLabel = Label(self.frame, text="Save directory:")
+		self.saveDirectoryEntry = Entry(self.frame)
+		self.saveDirectoryEntry.insert(0, "" + directory + "")
+		self.saveButton = Button(self.frame, text="Save", command=self.writeDirectoryToDatabase)
+		self.backButton = Button(self.frame, text="Back", command=self.home)
+
+		# Position the widgets
+		self.saveDirectoryLabel.grid(row=0, sticky=E)
+		self.saveDirectoryEntry.grid(row=0, column=1)
+		self.saveButton.grid(row=1, columnspan=2)
+		self.backButton.grid(row=2, columnspan=2)
+
+	def clearFrame(self):
+		for widget in self.frame.winfo_children():
+			widget.destroy()
+
+	def writeCameraToDatabase(self):
+		name = self.nameEntry.get()
+		ip = self.ipEntry.get()
+		port = self.portEntry.get()
+		password = self.passwordEntry.get()
+		
+		c.execute('INSERT INTO cameras (name, ip, port, password) VALUES ("' + name + '", "' + ip + '", "' + port + '", "' + password + '");')
+		conn.commit()
+		self.home()
+
+	def writeDirectoryToDatabase(self):
+		directory = self.saveDirectoryEntry.get()
+		if (directory[len(directory) - 1] != "/"):
+			directory += "/"
+		c.execute('DELETE FROM save_directory')
+		c.execute('INSERT INTO save_directory (directory) VALUES ("' + directory + '");')
+		conn.commit()
+		self.home()
+				
+	def removeCamera(self, name):
+		c.execute('DELETE FROM cameras WHERE name = "' + name + '";')
+		conn.commit()
+		self.home()
+	
+	def startCameraFeed(self, name):
+		runCamera(name)
+	
+	def closeWindow(self):
+		root.destroy()
+
+# Connect to database
+conn = sqlite3.connect('/home/john/opencv_database.db')
+c = conn.cursor()
+
+# Create application window
+root = Tk()
+root.title("security.py")
+root.minsize(width=640, height=480)
+myApp = MyApp(root)
+root.mainloop()
